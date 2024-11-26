a71e77c3a68b403f29d3718b6869db80523cd3c5
taetae98coding <taetae98coding@gmail.com>
2024-11-26 19:13:00 +0900

    init

---
+++ CodeLab/CodeLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeLabApp.swift
+//  CodeLab
+//
+//  Created by AL01992352 on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeLab
+//
+//  Created by AL01992352 on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
